9b09f7b606fad66bff764de95c32073811df0e9c
Vinícius <vinicius.pires@apotiguar.com.br>
2025-07-02 15:00:19 -0300

    Subindo api da clinica

---
+++ src/main/java/com/vinicius/clinical_med_crud/ClinicalMedCrudApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.clinical_med_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalMedCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalMedCrudApplication.class, args);
+	}
+
+}
